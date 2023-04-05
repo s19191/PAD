444f6ec0d6c2da9f461c63cbd11ed12914fed687
s19191 <s19191@pjwstk.edu.pl>
2023-04-06 01:18:04 +0200

    start c03

---
+++ c03/homework/main.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+file = pd.read_csv('PAD_03.csv', delimiter=";")
+print(file)
---
+++ c03/main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+file = pd.read_csv('salaries.csv')
+for column in file:
+    for line in fi
+    print(line)
