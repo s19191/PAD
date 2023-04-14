505c0111ac726449446d14fb71ebcc580fbb161d
s19191 <s19191@pjwstk.edu.pl>
2023-04-14 18:17:14 +0200

    finish

--- c04/homework/main.py
+++ c04/homework/main.py
@@ -9,27 +9,24 @@ task2 = np.genfromtxt('Task_2.csv', delimiter=';')
 print('Eigenvectors, and eigenvalues for the matrix')
 print(np.linalg.eig(task2))
 print(np.linalg.eigvals(task2))
+
 print('The inverse of the matrix')
 print(np.linalg.inv(task2))
 
-df = pd.read_csv('Task_3.csv', delimiter=";")
-df = df.iloc[:, :-4]
-df['DateTime'] = pd.to_datetime(df['DateTime'])
-df['Date'] = pd.to_datetime(df['DateTime'].dt.date)
-print(df)
-df2 = pd.DataFrame(df.groupby(['Date', 'DoctorID', 'Type', 'City'], as_index=False).first())
+task3 = pd.read_csv('Task_3.csv', delimiter=";", usecols=['DateTime', 'DoctorID', 'Type', 'City'])
+task3['DateTime'] = pd.to_datetime(task3['DateTime'])
+task3['Date'] = pd.to_datetime(task3['DateTime'].dt.date)
 
-print(df2)
+groupedTask3 = pd.DataFrame(task3.groupby(['Date', 'DoctorID', 'Type', 'City'], as_index=False).first())
 
-df2 = df2.iloc[:, 1:]
-df2['HowLong'] = df.groupby(['Date', 'DoctorID', 'Type', 'City'], as_index=False).count()['DateTime']
-df2['HowLong'] = pd.to_timedelta(df2['HowLong'], unit='m')
-df2['hour'] = df2['DateTime'] + df2['HowLong']
-df2['hour'] = df2['hour'].dt.time
-df2['DateTime'] = df2['DateTime'].astype(str) + ' - ' + df2['hour'].astype(str)
-df2 = df2.iloc[:, :-2]
-col = df2.pop('DateTime')
-df2.insert(0, col.name, col)
-df2.to_csv('out.csv', index=False)
-# print(df2)
+groupedTask3['HowLong'] = task3.groupby(['Date', 'DoctorID', 'Type', 'City'], as_index=False).count()['DateTime']
+groupedTask3['HowLong'] = pd.to_timedelta(groupedTask3['HowLong'], unit='m')
+groupedTask3['hour'] = groupedTask3['DateTime'] + groupedTask3['HowLong']
+groupedTask3['hour'] = groupedTask3['hour'].dt.time
+groupedTask3['DateTime'] = groupedTask3['DateTime'].astype(str) + ' - ' + groupedTask3['hour'].astype(str)
+groupedTask3 = groupedTask3.iloc[:, :-2]
 
+groupedTask3.to_csv('Task_3_output.csv', index=False)
+
+print('Doctors availability')
+print(groupedTask3)
