ec0e03c50930475761fc5b13ffc2391b9bb99167
s19191 <s19191@pjwstk.edu.pl>
2023-05-20 11:33:34 +0200

    finished

--- c08/main.py
+++ c08/main.py
@@ -1,25 +1,59 @@
+import matplotlib.pyplot as plt
 import pandas as pd
-import statsmodels.formula.api as smf
-from statsmodels.stats.weightstats import ttest_ind
-import plotly.graph_objects as go
 import plotly.express as px
-
-# z1
+import seaborn as sns
+import statsmodels.formula.api as smf
 
 df = pd.read_csv('PAD_09_PD.csv', delimiter=";")
-income_df = df.iloc[:, [0, 2]]
-male = income_df.query('Gender == "Male"')['Annual_Income']
-female = income_df.query('Gender == "Female"')['Annual_Income']
-res = ttest_ind(male, female)
-# p value > 0,05 nie możemy odrzucić hipotezy zerowej
-print(res)
-
-# 2
+df.rename(columns={'Spending Score (1-100)':'Spending_Score','Annual Income (k$)':'Annual_Income'}, inplace=True,)
+df['Gender'] = df['Gender'].apply(lambda x: x.replace('Male', '0'))
+df['Gender'] = df['Gender'].apply(lambda x: x.replace('Female', '1'))
 
 model = smf.ols(formula='Spending_Score ~ Gender + Age + Annual_Income', data=df).fit()
+
+plt.clf()
+sns.set()
+corr_matrix = df.corr()
+print(corr_matrix)
+heatMapDf = pd.DataFrame(corr_matrix)
+ax = sns.heatmap(corr_matrix)
+plt.show()
+
 print(model.summary())
-print(df.corr())
-fig = px.imshow(df.corr())
-fig.show()
-model2 = smf.ols(formula='Spending_Score ~ + Age + Annual_Income', data=df).fit()
-print(model2.summary())
+
+print('P values: ', model.pvalues.values)
+print('Coefficients: ', model.params.values)
+print('Standard dev: ', model.bse.values)
+
+px.scatter(df,'Annual_Income','Spending_Score','Gender',title='Spending score with Gender and Annual income').show()
+
+px.scatter(df,'Annual_Income','Spending_Score','Age',title='Spending score with Age and Annual income').show()
+
+px.scatter(df,'Age','Spending_Score','Gender',title='Spending score with Age and Gender').show()
+
+px.scatter(df,'Age','Annual_Income','Gender',title='Annual income with Age and Gender').show()
+
+pval = model.pvalues.values
+print('P value max: ', pval.max())
+print('P value max: ', pval.min())
+modelNoGender = smf.ols(formula='Spending_Score ~ Age + Annual_Income', data=df).fit()
+print(modelNoGender.summary())
+
+px.scatter(df,'Annual_Income','Spending_Score',title='Spending score with Annual income').show()
+
+px.scatter(df,'Annual_Income','Spending_Score','Age',title='Spending score with Age and Annual income').show()
+
+px.scatter(df,'Age','Spending_Score',title='Spending score with Age').show()
+
+px.scatter(df,'Age','Annual_Income',title='Annual income with Age').show()
+
+modelNoAnnualIncome = smf.ols(formula='Spending_Score ~ Age + Gender', data=df).fit()
+print(modelNoAnnualIncome.summary())
+
+px.scatter(df,'Gender','Spending_Score',title='Spending score with Annual income').show()
+
+px.scatter(df,'Gender','Spending_Score','Age',title='Spending score with Age and Annual income').show()
+
+px.scatter(df,'Age','Spending_Score',title='Spending score with Age').show()
+
+px.scatter(df,'Age','Gender',title='Annual income with Age').show()
