7aa3a8ee6144d451baaf7c0877f922b0b45e375d
kfaff <s19191@pjwstk.edu.pl>
2023-05-26 15:38:08 +0200

    finished

--- c09/main.py
+++ c09/main.py
@@ -1,31 +1,53 @@
 import pandas as pd
 from dash import Dash, dcc, html, dash_table
 from dash.dependencies import Input, Output
+import plotly.express as px
 
-df = pd.read_csv('winequelity.csv', delimiter=",")
+df = pd.read_csv('winequelity.csv')
 
 app = Dash(__name__)
 
-app.layout = dash_table.DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns])
+app.layout = html.Div([
+    html.H1("Wine Quality Analysis"),
 
-app.layout = html.Div(children=[
-    html.Div([
-        html.Label([
-            'Select model'
-        ]),
-        dcc.RadioItems(['Regression', 'Classification'], id='model'),
-        html.Div(id='output'),
-
-    ], style={'color': 'black'}
+    html.H3("Select a model type:"),
+    dcc.RadioItems(
+        id='model-type',
+        options=[
+            {'label': 'Regression', 'value': 'regression'},
+            {'label': 'Classification', 'value': 'classification'}
+        ],
+        value='regression',
+        labelStyle={'display': 'inline-block'}
     ),
+
+    html.H3("Select a variable:"),
+    dcc.Dropdown(
+        id='variable-dropdown',
+        options=[{'label': col, 'value': col} for col in df.columns],
+        value=df.columns[1]
+    ),
+
+    html.Div(id='graph-container'),
 
     dash_table.DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns])
 ])
 
+
+@app.callback(
+    Output('graph-container', 'children'),
+    [Input('model-type', 'value'),
+     Input('variable-dropdown', 'value')]
+)
+def display_graph(model_type, variable):
+    if model_type == 'regression':
+        fig = px.scatter(df, x=variable, y='pH', color='quality')
+        fig.update_layout(title=f"Regression: pH with {variable}")
+    else:
+        fig = px.histogram(df, x=variable, color='target')
+        fig.update_layout(title=f"Classification: Target with {variable}")
+
+    return dcc.Graph(figure=fig)
+
+
 app.run_server(debug=True)
-
-
-# @app.callback(Output(component_id='output', component_property='children'),
-#               Input(component_id='model', component_property='value'))
-# def update_output_div(input_value):
-#     return f'Output: {input_value}'
